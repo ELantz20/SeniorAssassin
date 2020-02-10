e6fce6f9c8c1da73d19bbc5ee01127b14048e607
elena <EKline20@holynames-sea.org>
2020-02-10 08:58:43 -0800

    keep on working on game logic

--- src/main/java/hello/Game.java
+++ src/main/java/hello/Game.java
@@ -7,7 +7,26 @@ public class Game {
 
     public void main (String [] args){
 
+        // initial assignments
+        for(int x = 0; x <masterList.size(); x++){
+            masterList.get(x).assign(masterList.get(x+1));
+            if(x == masterList.size()-1){
+                masterList.get(x).assign(masterList.get(0));
+            }
+        }
+
         while(masterList.size() != 1 ){
+
+            //checking "life status"
+            for(int x = 0; x <masterList.size(); x++){
+                if(masterList.get(x).getLifeStatus() == false){
+                    masterList.get(x).assign(masterList.get(x+1));
+                }
+
+                if(x == masterList.size()-1){
+                    masterList.get(x).assign(masterList.get(0));
+                }
+            }
 
             // "assigning"
             for(int x = 0; x <masterList.size(); x++){
