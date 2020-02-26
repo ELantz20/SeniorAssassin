a238752f301edf0c54e9b0d8d56610d24f77261b
carmenpolzin <cpolzin20@holynames-sea.org>
2020-02-26 07:25:44 -0800

    trying new table in database

---
+++ src/main/java/hello/populateTable.java
@@ -0,0 +1,19 @@
+package hello;
+import java.util.ArrayList;
+
+public class populateTable {
+
+    String name = "";
+    int studentID;
+    int peopleStillIn = 10;
+
+
+    public void main(String[] args) {
+
+        for (int i = 0; i < peopleStillIn; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
